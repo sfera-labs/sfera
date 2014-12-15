8111b48c74e7f94862f660e82aacda2135551e6f
Giampiero <giampiero.baggiani@gmail.com>
2014-12-15 12:44:23 +0100

    moved antlr files

--- src/main/antlr/run.sh
+++ src/main/antlr/run.sh
@@ -3,5 +3,5 @@ export CLASSPATH=".:/usr/local/lib/antlr-4.3-complete.jar"
 alias antlr4='java -jar /usr/local/lib/antlr-4.3-complete.jar'
 alias grun='java org.antlr.v4.runtime.misc.TestRig'
 antlr4 -package cc.sferalabs.sfera.script.parser SferaScriptGrammar.g4
-javac -d . SferaScriptGrammar*.java
-grun cc.sferalabs.sfera.script.parser.SferaScriptGrammar parse -gui ../../../../../scripts/test.ev
+#javac -d . SferaScriptGrammar*.java
+#grun cc.sferalabs.sfera.script.parser.SferaScriptGrammar parse -gui ../../../../../scripts/test.ev
