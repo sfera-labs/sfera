fd85bcc7590a7c984f285a367e94e9e67dd5a103
Giampiero <giampiero.baggiani@gmail.com>
2015-02-06 15:59:42 +0100

    String comparison on value.toString()

--- src/main/java/cc/sferalabs/sfera/script/TriggerCondition.java
+++ src/main/java/cc/sferalabs/sfera/script/TriggerCondition.java
@@ -20,7 +20,7 @@ import cc.sferalabs.sfera.script.parser.SferaScriptGrammarParser.UnknownComparis
 public class TriggerCondition {
 
 	private final TriggerContext condition;
-	
+
 	/**
 	 * 
 	 * @param condition
@@ -33,7 +33,7 @@ public class TriggerCondition {
 	 * 
 	 * @param event
 	 * @return
-	 * @throws Exception 
+	 * @throws Exception
 	 */
 	public boolean eval(Event event) throws Exception {
 		return eval(condition.orExpression(), event);
@@ -44,16 +44,16 @@ public class TriggerCondition {
 	 * @param ctx
 	 * @param event
 	 * @return
-	 * @throws Exception 
+	 * @throws Exception
 	 */
 	private boolean eval(OrExpressionContext ctx, Event event) throws Exception {
 		List<AndExpressionContext> ands = ctx.andExpression();
-		
+
 		boolean res = eval(ands.get(0), event);
 		for (int i = 1; i < ands.size(); i++) {
 			res = res || eval(ands.get(i), event);
 		}
-		
+
 		return res;
 	}
 
@@ -62,16 +62,17 @@ public class TriggerCondition {
 	 * @param ctx
 	 * @param event
 	 * @return
-	 * @throws Exception 
+	 * @throws Exception
 	 */
-	private boolean eval(AndExpressionContext ctx, Event event) throws Exception {
+	private boolean eval(AndExpressionContext ctx, Event event)
+			throws Exception {
 		List<NotExpressionContext> nots = ctx.notExpression();
-		
+
 		boolean res = eval(nots.get(0), event);
 		for (int i = 1; i < nots.size(); i++) {
 			res = res && eval(nots.get(i), event);
 		}
-		
+
 		return res;
 	}
 
@@ -80,9 +81,10 @@ public class TriggerCondition {
 	 * @param ctx
 	 * @param event
 	 * @return
-	 * @throws Exception 
+	 * @throws Exception
 	 */
-	private boolean eval(NotExpressionContext ctx, Event event) throws Exception {
+	private boolean eval(NotExpressionContext ctx, Event event)
+			throws Exception {
 		if (ctx.NOT() != null) {
 			return !eval(ctx.atomExpression(), event);
 		} else {
@@ -95,9 +97,10 @@ public class TriggerCondition {
 	 * @param ctx
 	 * @param event
 	 * @return
-	 * @throws Exception 
+	 * @throws Exception
 	 */
-	private boolean eval(AtomExpressionContext ctx, Event event) throws Exception {
+	private boolean eval(AtomExpressionContext ctx, Event event)
+			throws Exception {
 		if (ctx.event() != null) {
 			return eval(ctx.event(), event);
 		} else {
@@ -110,7 +113,7 @@ public class TriggerCondition {
 	 * @param ctx
 	 * @param event
 	 * @return
-	 * @throws Exception 
+	 * @throws Exception
 	 */
 	private boolean eval(EventContext ctx, Event event) throws Exception {
 		if (ctx.stableEvent() != null) {
@@ -133,26 +136,25 @@ public class TriggerCondition {
 			if (eventId.length() == condition.length()) { // equal
 				return true;
 			}
-			
-			if (eventId.charAt(condition.length()) == '.' ||
-					eventId.charAt(condition.length()) == '(') {
+
+			if (eventId.charAt(condition.length()) == '.'
+					|| eventId.charAt(condition.length()) == '(') {
 				/*
-				 *  meaning: 
-				 *  eventId.startsWith(condition + ".") || 
-				 *  	eventId.startsWith(condition + "(")
+				 * meaning: eventId.startsWith(condition + ".") ||
+				 * eventId.startsWith(condition + "(")
 				 */
 				return true;
 			}
 		}
-			
+
 		return false;
 	}
-	
+
 	/**
 	 * 
 	 * @param ctx
 	 * @return
-	 * @throws Exception 
+	 * @throws Exception
 	 */
 	private boolean eval(StableEventContext ctx) throws Exception {
 		if (ctx.stringComparison() != null) {
@@ -170,96 +172,101 @@ public class TriggerCondition {
 	 * 
 	 * @param ctx
 	 * @return
-	 * @throws Exception 
+	 * @throws Exception
 	 */
 	private boolean eval(StringComparisonContext ctx) throws Exception {
 		Object value = Bus.getValueOf(ctx.FinalNodeId().getText());
-		
+
 		if (value == null) {
 			return false;
 		}
-		
-		if (!(value instanceof String)) {
-			int line = ctx.getStart().getLine();
-			throw new Exception("line " + line + ": Type error: " + ctx.FinalNodeId().getText() + " not a string");
-		}
-		
+
+		String txtValue = value.toString();
+
 		String literal = ctx.StringLiteral().getText();
 		literal = literal.substring(1, literal.length() - 1);
-		
+
 		if (ctx.ET() != null) {
-			return literal.equals(value);
+			return literal.equals(txtValue);
 		} else if (ctx.NE() != null) {
-			return !literal.equals(value);
+			return !literal.equals(txtValue);
 		} else if (ctx.GT() != null) {
-			return ((String) value).compareTo(literal) > 0;
+			return txtValue.compareTo(literal) > 0;
 		} else if (ctx.LT() != null) {
-			return ((String) value).compareTo(literal) < 0;
+			return txtValue.compareTo(literal) < 0;
 		} else if (ctx.GE() != null) {
-			return ((String) value).compareTo(literal) >= 0;
+			return txtValue.compareTo(literal) >= 0;
 		} else { // LE
-			return ((String) value).compareTo(literal) <= 0;
+			return txtValue.compareTo(literal) <= 0;
 		}
 	}
-	
+
 	/**
 	 * 
 	 * @param ctx
 	 * @return
-	 * @throws Exception 
+	 * @throws Exception
 	 */
 	private boolean eval(NumberComparisonContext ctx) throws Exception {
 		Object value = Bus.getValueOf(ctx.FinalNodeId().getText());
-		
+
 		if (value == null) {
 			return false;
 		}
-		
+
 		if (!(value instanceof Double)) {
 			int line = ctx.getStart().getLine();
-			throw new Exception("line " + line + ": Type error: " + ctx.FinalNodeId().getText() + " not a number");
+			throw new Exception("line " + line + ": Type error: "
+					+ ctx.FinalNodeId().getText() + " not a number");
 		}
-		
+
 		if (ctx.ET() != null) {
-			return (Double) value == Double.parseDouble(ctx.NumberLiteral().getText());
+			return (Double) value == Double.parseDouble(ctx.NumberLiteral()
+					.getText());
 		} else if (ctx.NE() != null) {
-			return (Double) value != Double.parseDouble(ctx.NumberLiteral().getText());
+			return (Double) value != Double.parseDouble(ctx.NumberLiteral()
+					.getText());
 		} else if (ctx.GT() != null) {
-			return (Double) value > Double.parseDouble(ctx.NumberLiteral().getText());
+			return (Double) value > Double.parseDouble(ctx.NumberLiteral()
+					.getText());
 		} else if (ctx.LT() != null) {
-			return (Double) value < Double.parseDouble(ctx.NumberLiteral().getText());
+			return (Double) value < Double.parseDouble(ctx.NumberLiteral()
+					.getText());
 		} else if (ctx.GE() != null) {
-			return (Double) value >= Double.parseDouble(ctx.NumberLiteral().getText());
+			return (Double) value >= Double.parseDouble(ctx.NumberLiteral()
+					.getText());
 		} else { // LE
-			return (Double) value <= Double.parseDouble(ctx.NumberLiteral().getText());
+			return (Double) value <= Double.parseDouble(ctx.NumberLiteral()
+					.getText());
 		}
 	}
-	
+
 	/**
 	 * 
 	 * @param ctx
 	 * @return
-	 * @throws Exception 
+	 * @throws Exception
 	 */
 	private boolean eval(BooleanComparisonContext ctx) throws Exception {
 		Object value = Bus.getValueOf(ctx.FinalNodeId().getText());
-		
+
 		if (value == null) {
 			return false;
 		}
-		
+
 		if (!(value instanceof Boolean)) {
 			int line = ctx.getStart().getLine();
-			throw new Exception("line " + line + ": Type error: " + ctx.FinalNodeId().getText() + " not a boolean");
+			throw new Exception("line " + line + ": Type error: "
+					+ ctx.FinalNodeId().getText() + " not a boolean");
 		}
-		
+
 		if (ctx.ET() != null) {
 			return Boolean.parseBoolean(ctx.BooleanLiteral().getText()) == (Boolean) value;
 		} else { // NE
 			return Boolean.parseBoolean(ctx.BooleanLiteral().getText()) != (Boolean) value;
 		}
 	}
-	
+
 	/**
 	 * 
 	 * @param ctx
@@ -267,7 +274,7 @@ public class TriggerCondition {
 	 */
 	private boolean eval(UnknownComparisonContext ctx) {
 		Object value = Bus.getValueOf(ctx.FinalNodeId().getText());
-		
+
 		if (ctx.ET() != null) {
 			return value == null;
 		} else { // NE
