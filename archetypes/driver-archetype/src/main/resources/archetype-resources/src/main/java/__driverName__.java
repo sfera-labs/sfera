7d008e10f7622e31eddbaa7d0ba374e29f56bf7c
Giampiero <giampiero7@users.noreply.github.com>
2015-06-10 09:33:13 +0200

    Created driver archetype

---
+++ archetypes/driver-archetype/src/main/resources/archetype-resources/src/main/java/__driverName__.java
@@ -0,0 +1,32 @@
+package ${package};
+
+import cc.sferalabs.sfera.core.Configuration;
+import cc.sferalabs.sfera.drivers.Driver;
+
+public class ${driverName} extends Driver {
+
+	public ${driverName}(String id) {
+		super(id);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected boolean onInit(Configuration configuration)
+			throws InterruptedException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	protected boolean loop() throws InterruptedException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	protected void onQuit() throws InterruptedException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ archetypes/driver-archetype/src/main/resources/archetype-resources/src/main/java/events/__driverName__Event.java
@@ -0,0 +1,7 @@
+package ${package}.events;
+
+import cc.sferalabs.sfera.events.Event;
+
+public interface ${driverName}Event extends Event {
+
+}
