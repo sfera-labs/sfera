5d722624b2b568894404b4c74442164602425bc4
Giampiero Baggiani <giampiero7@users.noreply.github.com>
2024-01-17 20:45:08 +0100

    Handling string conversion errors for JsonMessages

--- src/main/java/cc/sferalabs/sfera/web/api/JsonMessage.java
+++ src/main/java/cc/sferalabs/sfera/web/api/JsonMessage.java
@@ -22,6 +22,9 @@
 
 package cc.sferalabs.sfera.web.api;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
 
 import org.json.JSONException;
@@ -49,8 +52,7 @@ public class JsonMessage {
 	/**
 	 * Constructs a JSON message parsing the specified string.
 	 * 
-	 * @param json
-	 *            the string to parse
+	 * @param json the string to parse
 	 */
 	public JsonMessage(String json) {
 		this(new JSONObject(json));
@@ -64,13 +66,11 @@ public class JsonMessage {
 	}
 
 	/**
-	 * Sets the attribute of this message specified by {@code key} to the
-	 * specified {@code value}.
+	 * Sets the attribute of this message specified by {@code key} to the specified
+	 * {@code value}.
 	 * 
-	 * @param key
-	 *            attribute to set
-	 * @param value
-	 *            value for the attribute to set
+	 * @param key   attribute to set
+	 * @param value value for the attribute to set
 	 */
 	public void put(String key, Object value) {
 		if (value == null) {
@@ -80,17 +80,14 @@ public class JsonMessage {
 	}
 
 	/**
-	 * Returns the value of the attribute of this message specified by
-	 * {@code key}, or {@code null} if this message does not have such
-	 * attribute.
+	 * Returns the value of the attribute of this message specified by {@code key},
+	 * or {@code null} if this message does not have such attribute.
 	 * 
-	 * @param <T>
-	 *            Type to cast the returned value to
+	 * @param <T> Type to cast the returned value to
 	 * 
-	 * @param key
-	 *            attribute to get
-	 * @return the value of the attribute, or {@code null} if no such attribute
-	 *         is found
+	 * @param key attribute to get
+	 * @return the value of the attribute, or {@code null} if no such attribute is
+	 *         found
 	 */
 	@SuppressWarnings("unchecked")
 	public <T> T get(String key) {
@@ -110,11 +107,69 @@ public class JsonMessage {
 	 * Returns this JSON message in string format.
 	 * 
 	 * @return this JSON message in string format
-	 * @throws JSONException
-	 *             If the object contains an invalid number
+	 * @throws JSONException If the object contains an invalid number
 	 */
 	public String toJsonString() throws JSONException {
-		return obj.toString(0);
+		try {
+			return obj.toString(0);
+		} catch (Throwable t) {
+			if (t instanceof JSONException) {
+				throw t;
+			}
+			throw getError(obj.toMap(), null, t);
+		}
+	}
+
+	/**
+	 * @param map
+	 * @param key
+	 * @param err
+	 * @return
+	 */
+	private static JSONException getError(Map<?, ?> map, String key, Throwable err) {
+		Object k = null;
+		Object v = null;
+		String vStr = null;
+		try {
+			for (Entry<?, ?> me : map.entrySet()) {
+				k = me.getKey();
+				v = me.getValue();
+				try {
+					HashMap<String, Object> l = new HashMap<>();
+					l.put(k.toString(), v);
+					JSONObject jo = new JSONObject(l);
+
+					jo.toString(0);
+
+					k = null;
+					v = null;
+				} catch (Throwable t) {
+					break;
+				}
+			}
+			if (v != null) {
+				key = key == null ? k.toString() : (key + " > " + k);
+				map = null;
+				if (v instanceof Map) {
+					map = (Map<?, ?>) v;
+				}
+				if (v instanceof JSONObject) {
+					map = ((JSONObject) v).toMap();
+				}
+				if (map != null) {
+					return getError(map, key, err);
+				}
+			}
+		} catch (Throwable t) {
+		}
+
+		try {
+			vStr = v.toString();
+		} catch (Throwable ve) {
+			vStr = null;
+		}
+
+		return new JSONException("Error writing JSON value for '" + key + "' (" + vStr + "): " + err, err);
 	}
 
 }
