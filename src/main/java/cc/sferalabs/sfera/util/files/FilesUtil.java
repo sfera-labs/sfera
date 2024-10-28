3ef27694e79b9f3dee3766afce144836fd0d77eb
Giampiero Baggiani <giampiero7@users.noreply.github.com>
2024-10-28 15:36:53 +0100

    Java 17 compatibility

--- src/main/java/cc/sferalabs/sfera/util/files/FilesUtil.java
+++ src/main/java/cc/sferalabs/sfera/util/files/FilesUtil.java
@@ -168,7 +168,7 @@ public abstract class FilesUtil {
 	 *             if an I/O error occurs
 	 */
 	public static void unzip(Path source, Path dir, CopyOption... options) throws IOException {
-		try (FileSystem zipFs = FileSystems.newFileSystem(source, null)) {
+		try (FileSystem zipFs = FileSystems.newFileSystem(source, null, null)) {
 			Files.walkFileTree(zipFs.getPath("/"), new SimpleFileVisitor<Path>() {
 
 				/**
